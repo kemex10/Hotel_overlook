f52f3094bdcaab4345f0abc6b8623322be755492
kemex10 <61971140+kemex10@users.noreply.github.com>
2021-08-19 16:28:23 +0200

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,48 @@
+import './main.scss';
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom'
+import { Navigation } from './components/Navigation/Navigation';
+import { Footer } from './components/Footer/Footer';
+import { FrontPage } from './pages/FrontPage/FrontPage';
+import { HotelsDestinationsPage } from './pages/HotelsDestinationsPage/HotelsDestinationsPage';
+import { LoginPage } from './pages/LoginPage/LoginPage';
+import { useEffect, useState } from 'react';
+
+function App() {
+
+  const [loginData, setLoginData] = useState([])
+
+  useEffect(() => {
+    if (sessionStorage.getItem('token')) {
+      setLoginData(JSON.parse(sessionStorage.getItem('token')))
+    }
+  }, [])
+
+  return (
+    <Router>
+      <Navigation loginData={loginData} />
+
+      <Switch>
+        <Route exact path="/">
+          <FrontPage />
+        </Route>
+
+        <Route path="/hoteller-og-Destinationer">
+          <HotelsDestinationsPage />
+        </Route>
+
+
+        <Route path="/login">
+          <LoginPage loginData={loginData} setLoginData={setLoginData} />
+        </Route>
+
+        <Route path="/">
+          <p>404 - siden findes ikke.</p>
+        </Route>
+      </Switch>
+
+      <Footer />
+    </Router>
+  );
+}
+
+export default App;
